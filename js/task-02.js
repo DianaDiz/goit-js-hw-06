c7c7c1df811bea0cf68076281c19b8305d36c693
DianaDiz <dianaandpir@gmail.com>
2022-09-09 16:37:07 +0300

    додає рішення 2го завдання

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,39 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const itemPotatoesEl = document.createElement('li');
+console.log(itemPotatoesEl);
+itemPotatoesEl.classList.add('item');
+itemPotatoesEl.textContent = 'Potatoes';
+
+const itemMushroomsEl = document.createElement('li');
+console.log(itemMushroomsEl);
+itemMushroomsEl.classList.add('item');
+itemMushroomsEl.textContent = 'Mushrooms';
+
+const itemGarlicEl = document.createElement('li');
+console.log(itemGarlicEl);
+itemGarlicEl.classList.add('item');
+itemGarlicEl.textContent = 'Garlic';
+
+const itemTomatosEl = document.createElement('li');
+console.log(itemTomatosEl);
+itemTomatosEl.classList.add('item');
+itemTomatosEl.textContent = 'Tomatos';
+
+const itemHerbsEl = document.createElement('li');
+console.log(itemHerbsEl);
+itemHerbsEl.classList.add('item');
+itemHerbsEl.textContent = 'Herbs';
+
+const itemCondimentsEl = document.createElement('li');
+console.log(itemCondimentsEl);
+itemCondimentsEl.classList.add('item');
+itemCondimentsEl.textContent = 'Condiments';
+
+const list = document.querySelector('#ingredients');
+
+list.append(itemPotatoesEl, itemMushroomsEl, itemGarlicEl, itemTomatosEl, itemHerbsEl, itemCondimentsEl);
+
+
