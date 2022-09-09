49bb0b6589d10d36b1e033b60ba3c0414ef08f01
DianaDiz <dianaandpir@gmail.com>
2022-09-09 15:47:24 +0300

    завантажує стартові файли

---
+++ js/task-01.js
@@ -0,0 +1,14 @@
+const categoriesEL = document.querySelector('#categories');
+console.log(`Number of categories: ${categoriesEL.children.length}`);
+
+const items = document.querySelectorAll('.item');
+
+items.forEach((item) => {
+    console.log(`Category: ${item.firstElementChild.textContent}`);
+    const quantityEl = item.querySelectorAll('li');
+    const elementsEl = document.querySelector('.item');
+    console.log(`Elements: ${quantityEl.length}`);
+    
+});
+
+
