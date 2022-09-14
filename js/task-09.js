5152d2ae0bb2edc1742f71143b288ce766f5b2df
DianaDiz <dianaandpir@gmail.com>
2022-09-14 16:02:04 +0300

    додає рішення 9го завдання

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,15 @@
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+};
+
+const body = document.querySelector('.widget');
+const btn = document.querySelector('.change-color');
+const span = document.querySelector('.color');
+
+
+function onBtnClick() {
+  body.style.backgroundColor = getRandomHexColor();
+  return span.textContent = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+btn.addEventListener('click', onBtnClick);
