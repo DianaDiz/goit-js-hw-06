2d6ae276173c6640d1e53e57c2355ddfc2212354
DianaDiz <dianaandpir@gmail.com>
2022-09-14 15:15:46 +0300

    додає рішення 8го завдання

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,20 @@
+const form = document.querySelector('.login-form');
 
+form.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(event) {
+    event.preventDefault();
+
+    const formElements = event.currentTarget.elements;
+    const email = formElements.email.value;
+    const password = formElements.password.value;
+    const FormData = {
+        email,
+        password,
+    };
+    console.log(FormData);
+    if (email === "" || password === "") {
+    return alert("Please fill in all the fields!");
+    };
+    form.reset();
+}
