0c082ca1025722b9e2aedb33715e5ff1f0cafa44
DianaDiz <dianaandpir@gmail.com>
2022-09-15 15:36:42 +0300

    додає виправлене рішення 2го завдання

--- js/task-02.js
+++ js/task-02.js
@@ -7,38 +7,19 @@ const ingredients = [
   'Condiments',
 ];
 
-const itemPotatoesEl = document.createElement('li');
-console.log(itemPotatoesEl);
-itemPotatoesEl.classList.add('item');
-itemPotatoesEl.textContent = 'Potatoes';
+const li = document.createElement('li');
+li.textContent = `${ingredients[0]}`;
+li.classList.add('item');
+console.log(li);
 
-const itemMushroomsEl = document.createElement('li');
-console.log(itemMushroomsEl);
-itemMushroomsEl.classList.add('item');
-itemMushroomsEl.textContent = 'Mushrooms';
+const array = [];
 
-const itemGarlicEl = document.createElement('li');
-console.log(itemGarlicEl);
-itemGarlicEl.classList.add('item');
-itemGarlicEl.textContent = 'Garlic';
+ingredients.forEach((element) => {
+  const li = document.createElement('li');
+  li.textContent = `${element}`;
+  return array.push(li);
+});
+console.log(array);
 
-const itemTomatosEl = document.createElement('li');
-console.log(itemTomatosEl);
-itemTomatosEl.classList.add('item');
-itemTomatosEl.textContent = 'Tomatos';
-
-const itemHerbsEl = document.createElement('li');
-console.log(itemHerbsEl);
-itemHerbsEl.classList.add('item');
-itemHerbsEl.textContent = 'Herbs';
-
-const itemCondimentsEl = document.createElement('li');
-console.log(itemCondimentsEl);
-itemCondimentsEl.classList.add('item');
-itemCondimentsEl.textContent = 'Condiments';
-
-const list = document.querySelector('#ingredients');
-
-list.append(itemPotatoesEl, itemMushroomsEl, itemGarlicEl, itemTomatosEl, itemHerbsEl, itemCondimentsEl);
-
-
+const ul = document.querySelector('#ingredients');
+ul.append(...array);
